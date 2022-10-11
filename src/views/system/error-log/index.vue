4479c58275591e50fd0b832080aa000290bab381
niexiangyuan <niexiangyuan@sedinbj>
2022-10-11 15:42:54 +0800

    去掉多余demo

--- src/views/system/error-log/index.vue
+++ src/views/system/error-log/index.vue
@@ -1,3 +1,7 @@
+<!--
+ * @Author: nxy
+ * @Date: 2022-10-09 09:56:08
+-->
 <template>
   <div class="p-4">
     <template v-for="src in imgList" :key="src">
@@ -12,9 +16,9 @@
         <a-button @click="fireResourceError" type="primary">
           {{ t('sys.errorLog.fireResourceError') }}
         </a-button>
-        <a-button @click="fireAjaxError" type="primary">
+        <!-- <a-button @click="fireAjaxError" type="primary">
           {{ t('sys.errorLog.fireAjaxError') }}
-        </a-button>
+        </a-button> -->
       </template>
       <template #bodyCell="{ column, record }">
         <template v-if="column.key === 'action'">
@@ -41,7 +45,6 @@
   import { useMessage } from '/@/hooks/web/useMessage';
   import { useI18n } from '/@/hooks/web/useI18n';
   import { useErrorLogStore } from '/@/store/modules/errorLog';
-  import { fireErrorApi } from '/@/api/demo/error';
   import { getColumns } from './data';
   import { cloneDeep } from 'lodash-es';
 
@@ -89,9 +92,5 @@
 
   function fireResourceError() {
     imgList.value.push(`${new Date().getTime()}.png`);
-  }
-
-  async function fireAjaxError() {
-    await fireErrorApi();
   }
 </script>
