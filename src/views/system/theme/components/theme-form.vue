c5cdd863aa49ac2e5b3f9afd0d0e9e0f223df7a0
niexiangyuan <niexiangyuan@sedinbj>
2022-10-19 22:19:15 +0800

    主题管理

---
+++ src/views/system/theme/components/theme-form.vue
@@ -0,0 +1,151 @@
+<template>
+  <a-form
+    layout="horizontal"
+    ref="formRef"
+    :model="form"
+    :rules="rules"
+    :label-col="{ md: { span: 4 }, sm: { span: 24 } }"
+    :wrapper-col="{ md: { span: 20 }, sm: { span: 24 } }"
+  >
+    <a-form-item label="主题模板" name="templateId">
+      <a-select
+        :disabled="disabledChangeTemplate"
+        v-model:value="form.templateId"
+        placeholder="请选择主题模板"
+        :options="templateList"
+        @change="getThemeAttributes"
+      />
+    </a-form-item>
+    <a-form-item label="主题名称:" name="themeName">
+      <a-input
+        v-model:value="form.themeName"
+        placeholder="请输入主题名称"
+        allow-clear
+        autocomplete="off"
+      />
+    </a-form-item>
+    <a-divider style="padding-bottom: 24px" />
+    <!--循环遍历主题的所有字段-->
+    <a-form-item
+      :label="templateField.fieldName"
+      :name="templateField.fieldCode"
+      v-for="templateField in templateFields"
+      :key="templateField"
+    >
+      <!--string类型自定义表单-->
+      <a-input
+        v-if="templateField.fieldType === 'string'"
+        v-model:value="form[templateField.fieldCode]"
+      />
+      <!--file类型自定义表单-->
+      <a-upload
+        v-else-if="templateField.fieldType === 'file'"
+        name="file"
+        :multiple="false"
+        :action="fileUploadUrl"
+        :headers="headers"
+        list-type="picture"
+        v-model:file-list="tempFileList[templateField.fieldCode]"
+        @change="handleFileChange($event, templateField.fieldCode)"
+      >
+        <a-button>
+          <upload-outlined />
+          上传图片
+        </a-button>
+      </a-upload>
+    </a-form-item>
+  </a-form>
+</template>
+
+<script lang="ts" setup>
+  import { onMounted, reactive, ref } from 'vue';
+  import { ThemeTemplateApi } from '/@/api/system/theme/ThemeTemplateApi';
+  import { FileUploadUrl as fileUploadUrlPrefix } from '/@/api/system/operation/FileApi';
+  import { useUserStore } from '/@/store/modules/user';
+  import { message } from 'ant-design-vue';
+
+  const props = defineProps<{
+    form: Object,
+    rules,
+    // 是否禁用主题
+    disabledChangeTemplate: Boolean,
+    // 临时存放文件列表（用来限制上传数量）
+    tempFileList: Object,
+    // 模板属性
+    templateFields: Array,
+  }>();
+
+  const emits = defineEmits<{
+    (e: 'getThemeAttributes', value: any): void;
+  }>();
+
+  const useStore = useUserStore();
+
+  // 模板列表
+  const templateList = ref<string[]>([]);
+
+  // 上传文件的url
+  const fileUploadUrl = ref<string>(`/api${fileUploadUrlPrefix}?secretFlag=N`);
+
+  // 上传文件时候要带header
+  const headers = reactive({
+    Authorization: useStore.getToken,
+  });
+
+  /**
+   * 获取搜索框焦点时，获取所有的主题模板
+   *
+   * @author fengshuonan
+   * @date 2021/12/29 10:08:31
+   */
+  const getThemeTemplates = async () => {
+    let templates = await ThemeTemplateApi.findList();
+    templateList.value = [];
+    for (let template of templates) {
+      if (template.statusFlag === 'Y') {
+        templateList.value.push({
+          value: template.templateId,
+          label: template.templateName,
+        });
+      }
+    }
+  };
+
+  onMounted(() => {
+    getThemeTemplates();
+  });
+
+  /**
+   * 点击选择模板时，查询系统模板详情
+   *
+   * @param value 模板id
+   * @author fengshuonan
+   * @date 2021/12/29 10:33:06
+   */
+  const getThemeAttributes = (value: any) => {
+    emits('getThemeAttributes', value);
+  };
+
+  /**
+   * 上传文件改变时的状态
+   *
+   * @param info 组件回调原有参数
+   * @param fieldCode 文件表单字段名称
+   * @author fengshuonan
+   * @date 2021/12/29 14:25:18
+   */
+  const handleFileChange = (info: any, fieldCode: any) => {
+    if (info.file.status === 'done') {
+      // 设置临时fileList的值
+      props.tempFileList[fieldCode] = [info.file];
+      // 将文件属性名和文件编码存入数组中
+      message.success(`${info.file.name} 图片上传成功`);
+      // 设置表单对应的文件属性的值
+      props.form[fieldCode] = info.file.response.data.fileId;
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} 图片上传失败`);
+    }
+  };
+</script>
+
+<style></style>
---
+++ src/views/system/theme/theme.vue
@@ -0,0 +1,206 @@
+<template>
+  <div class="guns-body">
+    <!-- 搜索表单 -->
+    <div class="block-interval">
+      <a-card :bordered="false">
+        <a-form layout="inline" :model="where">
+          <a-row>
+            <a-form-item label="主题名称:">
+              <a-input
+                v-model:value.trim="where.themeName"
+                placeholder="请输入主题名称"
+                allow-clear
+              />
+            </a-form-item>
+            <a-form-item class="ele-text-center">
+              <a-space>
+                <a-button type="primary" @click="reload">查询</a-button>
+                <a-button @click="reset">重置</a-button>
+              </a-space>
+            </a-form-item>
+          </a-row>
+        </a-form>
+      </a-card>
+    </div>
+
+    <!-- 表格 -->
+    <div>
+      <a-card :bordered="false" class="table-height">
+        <BasicTable
+          :canResize="false"
+          ref="tableRef"
+          :api="ThemeApi.findPage"
+          :where="where"
+          :columns="columns"
+          showTableSetting
+          rowKey="themeId"
+          :rowSelection="{
+            type: 'checkbox',
+            selectedRowKeys: checkedKeys,
+          }"
+        >
+          <!-- table上边工具栏 -->
+          <template #toolbar>
+            <div class="table-toolbar">
+              <a-space>
+                <a-button type="primary" @click="openEdit()">
+                  <template #icon>
+                    <plus-outlined />
+                  </template>
+                  <span>新建</span>
+                </a-button>
+              </a-space>
+            </div>
+          </template>
+
+          <template #bodyCell="{ column, record }">
+            <template v-if="column.dataIndex == 'themeName'">
+              <a @click="openEdit(record)">{{ record.themeName }}</a>
+            </template>
+
+            <!-- table列表状态栏 -->
+            <!-- 1是激活，2是禁用 -->
+            <template v-if="column.key === 'state'">
+              <a-switch
+                :checked="record.statusFlag === 'Y'"
+                @change="(checked) => editStatus(checked, record)"
+              />
+            </template>
+
+            <!-- table操作栏按钮 -->
+            <template v-else-if="column.key === 'action'">
+              <a-space>
+                <a @click="openEdit(record)">修改</a>
+                <a-divider type="vertical" />
+                <a-popconfirm title="确定要删除此主题吗？" @confirm="remove(record)">
+                  <a class="ele-text-danger">删除</a>
+                </a-popconfirm>
+              </a-space>
+            </template>
+          </template>
+        </BasicTable>
+      </a-card>
+    </div>
+
+    <!-- 编辑弹窗 -->
+    <theme-edit v-model:visible="showEdit" :data="current" @done="reload" v-if="showEdit" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { reactive, ref } from 'vue';
+  import { BasicTable } from '/@/components/Table';
+  import { message } from 'ant-design-vue';
+  import { ThemeApi } from '/@/api/system/theme/ThemeApi';
+  import ThemeEdit from './components/theme-edit.vue';
+
+  // 查询条件
+  const where = reactive({
+    themeName: '',
+  });
+
+  // 表格配置
+  const columns = ref<string[]>([
+    {
+      title: '主题名称',
+      dataIndex: 'themeName',
+      width: 160,
+    },
+    {
+      title: '主题模板',
+      dataIndex: 'templateName',
+      width: 160,
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'createTime',
+      width: 160,
+    },
+    {
+      title: '启用状态',
+      key: 'state',
+      dataIndex: 'statusFlag',
+      width: 100,
+      align: 'center',
+    },
+    {
+      title: '操作',
+      key: 'action',
+      width: 260,
+      align: 'center',
+    },
+  ]);
+
+  // 多选选中列表
+  const checkedKeys = ref<Array<string | number>>([]);
+
+  // 当前行数据
+  const current = ref<any>(null);
+
+  // 是否显示新增编辑弹框
+  const showEdit = ref<boolean>(false);
+
+  // ref
+  const tableRef = ref(null);
+
+  /**
+   * 点击查询
+   * @author: nxy
+   * @Date: 2022-10-12 09:38:18
+   */
+  const reload = () => {
+    checkedKeys.value = [];
+    tableRef.value.reload({ page: 1 });
+  };
+
+  /**
+   * 点击重置
+   * @author: nxy
+   * @Date: 2022-10-12 09:38:29
+   */
+  const reset = () => {
+    where.themeName = '';
+    reload();
+  };
+
+  /**
+   * 删除单个
+   *
+   * @author fengshuonan
+   * @date 2021/4/2 17:03
+   */
+  const remove = async (row: any) => {
+    const result = await ThemeApi.del({ themeId: row.themeId });
+    message.success(result.message);
+    reload();
+  };
+
+  /**
+   * 打开编辑页面
+   *
+   * @author fengshuonan
+   * @date 2021/6/15 23:14
+   */
+  const openEdit = (record: any) => {
+    current.value = record;
+    showEdit.value = true;
+  };
+
+  /**
+   * 修改主题状态
+   *
+   * @author fengshuonan
+   * @date 2021/12/21 11:30:07
+   */
+  const editStatus = async (checked: boolean, row: any) => {
+    const themeId = row.themeId;
+    // 职位状态：Y-启用，N-禁用
+    const statusFlag = checked ? 'Y' : 'N';
+    const result = await ThemeApi.updateThemeStatus({ themeId });
+    message.success(result.message);
+    row.statusFlag = statusFlag;
+    reload();
+  };
+</script>
+
+<style></style>
