31738d6fa3c2d6cc03be73ba67860cab5fee6704
ye_star <17705172417@189.cn>
2022-10-25 09:25:06 +0800

    初始化页TS改造

--- src/views/init/index.vue
+++ src/views/init/index.vue
@@ -33,94 +33,82 @@
   </a-spin>
 </template>
 
-<script lang="ts">
-  import { defineComponent, onMounted, reactive, toRefs } from 'vue';
+<script name="InitPage" lang="ts" setup>
+  import { onMounted, reactive, ref } from 'vue';
   import { SysConfigApi } from '/@/api/system/basedata/SysConfigApi';
   import { message } from 'ant-design-vue';
   import { router } from '/@/router';
   import { useUserStore } from '/@/store/modules/user';
+  import { ConfigInitItem, ConfigInitRequest } from '/@/api/system/basedata/model/SysConfigModel';
 
-  export default defineComponent({
-    name: 'InitPage',
-    components: {},
-    setup() {
-      let data = reactive({
-        // 页面加载中标识
-        loading: true,
-        // 表单数据
-        form: {},
-        // 系统初始化的系统配置列表
-        configList: [],
-      });
+  const userStore = useUserStore();
 
-      const userStore = useUserStore();
+  // 页面加载中标识
+  const loading = ref<boolean>(true);
+  // 表单数据
+  const form = reactive({});
+  // 系统初始化的系统配置列表
+  const configList = ref<ConfigInitItem[]>([]);
 
-      onMounted(async () => {
-        // 判断系统是否已经初始化过
-        let initConfigFlag = await SysConfigApi.getInitConfigFlag();
+  onMounted(async () => {
+    // 判断系统是否已经初始化过
+    let initConfigFlag = await SysConfigApi.getInitConfigFlag();
 
-        // 如果已经初始化过，则跳转到首页
-        if (initConfigFlag) {
-          router.push(userStore?.userInfo?.homePath);
-          return;
-        }
+    // 如果已经初始化过，则跳转到首页
+    if (initConfigFlag) {
+      router.push(userStore?.userInfo?.homePath ?? '/');
+      return;
+    }
 
-        // 关闭加载中
-        data.loading = false;
+    // 关闭加载中
+    loading.value = false;
 
-        // 获取需要初始化的系统配置
-        data.configList = await SysConfigApi.getInitConfigList();
-        for (const item of data.configList) {
-          // 替换主机名为url的访问地址
-          if (item.configCode === 'SYS_SERVER_DEPLOY_HOST') {
-            data.form[item.configCode] = item.configValue.replace(
-              'localhost:8080',
-              window.location.host + '/api',
-            );
-          } else if (item.configCode === 'WEB_SOCKET_WS_URL') {
-            data.form[item.configCode] = item.configValue.replace(
-              'localhost:8080',
-              window.location.host + '/api',
-            );
-          } else {
-            data.form[item.configCode] = item.configValue;
-          }
-        }
-      });
-
-      // 提交初始化配置
-      const onSubmit = async () => {
-        let params = { sysConfigs: data.form };
-        let result = await SysConfigApi.initConfig(params);
-        message.success(result.message);
-
-        // 跳转到首页
-        router.push(userStore?.userInfo?.homePath);
-      };
-
-      // 重置数据
-      const onReset = async () => {
-        // 加载中标识
-        data.loading = true;
-
-        // 获取需要初始化的系统配置
-        data.configList = await SysConfigApi.getInitConfigList();
-
-        // 加载中标识关闭
-        data.loading = false;
-
-        for (const item of data.configList) {
-          data.form[item.configCode] = item.configValue;
-        }
-      };
-
-      return {
-        ...toRefs(data),
-        onSubmit,
-        onReset,
-      };
-    },
+    // 获取需要初始化的系统配置
+    configList.value = await SysConfigApi.getInitConfigList();
+    for (const item of configList.value) {
+      // 替换主机名为url的访问地址
+      if (item.configCode === 'SYS_SERVER_DEPLOY_HOST') {
+        form[item.configCode] = item.configValue?.replace(
+          'localhost:8080',
+          window.location.host + '/api',
+        );
+      } else if (item.configCode === 'WEB_SOCKET_WS_URL') {
+        form[item.configCode] = item.configValue?.replace(
+          'localhost:8080',
+          window.location.host + '/api',
+        );
+      } else {
+        form[item.configCode] = item.configValue;
+      }
+    }
   });
+
+  // 提交初始化配置
+  const onSubmit = async () => {
+    let param: ConfigInitRequest = {};
+    param.sysConfigs = form;
+    let result = await SysConfigApi.initConfig(param);
+    message.success(result.message);
+
+    // 跳转到首页
+    router.push(userStore?.userInfo?.homePath ?? '/');
+  };
+
+  // 重置数据
+  const onReset = async () => {
+    // 加载中标识
+    loading.value = true;
+
+    // 获取需要初始化的系统配置
+    configList.value = await SysConfigApi.getInitConfigList();
+
+    // 加载中标识关闭
+    loading.value = false;
+
+    for (const item of configList.value) {
+      form[item.configCode] = item.configValue;
+    }
+  };
 </script>
 
 <style scoped>
